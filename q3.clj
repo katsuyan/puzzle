a47a5ddcb227e122f54481afc46d2907a4ed5021
Katsuya Tajima <zakichan1216@gmail.com>
2016-12-01 23:42:46 +0900

    comp q3

--- q3.clj
+++ q3.clj
@@ -1,8 +1,19 @@
 (def N 100)
+(def start-num 2)
 (def cards (for [x (range 1 (inc N)) y [false]] [x y]))
 
-(defn reverse-cars [cards num]
+(defn reverse-cards [cards num]
   (map #(if (= 0 (mod (first %) num))
             [(first %) (not (second %))]
             %)
        cards))
+
+(defn reverse [cards num]
+  (if (= num N)
+      (reverse-cards cards num)
+      (reverse (reverse-cards cards num) (inc num))))
+
+(defn get-answer []
+  (map #(first %)
+      (filter #(not (second %))
+              (reverse cards start-num))))
