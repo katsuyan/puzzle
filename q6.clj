70b096bc3679d67f9b158bb1b448095b61a82553
Katsuya Tajima <zakichan1216@gmail.com>
2017-02-26 08:40:18 +0900

    add leiningen

--- q6.clj
+++
@@ -1,9 +0,0 @@
-(defn check-recur [n start]
-  (cond (= n start) true
-        (= n 1) false
-        :else (if (even? n)
-                  (check-recur (/ n 2) start)
-                  (check-recur (+ (* n) 1) start))))
-
-(defn check [n]
-  (check-recur (+ (* n) 1) n))
---
+++ src/puzzule/q6.clj
@@ -0,0 +1,15 @@
+(defn check-recur [n start]
+  (cond (= n start) true
+        (= n 1) false
+        :else (if (even? n)
+                  (check-recur (/ n 2) start)
+                  (check-recur (+ (* n 3) 1) start))))
+
+(defn check [n]
+  (check-recur (+ (* n 3) 1) n))
+
+(defn get-answer [n]
+  (count (filter check (range 2 (+ n 1) 2))))
+
+(def n 10000)
+(def answer (get-answer n))
