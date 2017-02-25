b9329c586c02836534efbcd588b341b0c120647a
Katsuya Tajima <zakichan1216@gmail.com>
2017-02-26 08:53:34 +0900

    change readme

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-# puzzule
-
-A Clojure library designed to ... well, that part is up to you.
-
-## Usage
-
-FIXME
-
-## License
-
-Copyright © 2017 FIXME
-
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+# プログラマ脳を鍛える数学パズル in Clojure
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject puzzule "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "プログラマ脳を鍛える数学パズル in Clojure"
+  :url "http://zakiran.hatenablog.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
