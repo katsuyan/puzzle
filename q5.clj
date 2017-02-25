06a9068c88b1d10b2b1dc714718e2739d9792537
Katsuya Tajima <zakichan1216@gmail.com>
2017-02-25 22:05:32 +0900

    add q5 q6

---
+++ q5.clj
@@ -0,0 +1,16 @@
+(def coins [10 50 100 500])
+
+(defn exchange-recur [acc money coin-num]
+  (cond (or (< coin-num 0) (> acc 15)) 0
+        (= money 0) 1
+        :else (+ (exchange-recur (inc acc)
+                                 (- money (get coins coin-num))
+                                 coin-num)
+                  (exchange-recur acc
+                                  money
+                                  (dec coin-num)))))
+
+(defn exchange []
+  (exchange-recur 0 1000 3))
+
+(exchange)
