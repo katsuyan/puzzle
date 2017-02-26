6d14b27070978ae8506df4dcefc559fd0f4720e0
Katsuya Tajima <zakichan1216@gmail.com>
2017-02-26 11:17:45 +0900

    add q7

--- src/puzzule/q7.clj
+++ src/puzzule/q7.clj
@@ -1,2 +1,32 @@
 (ns puzzule.q7
-  (:require [clj-time.core :as t]))
+  (:require [clj-time.core :as t]
+            [clj-time.format :as f]
+            [clojure.string :as string]))
+
+(defn- parse-int [x]
+  (Integer/parseInt x))
+
+(defn- int-to-binary-string [n]
+  (Integer/toBinaryString n))
+
+(defn- binary-string-to-int [n]
+  (Integer/parseInt n 2))
+
+(defn reverse-same-binary? [bin]
+  (= bin (string/reverse bin)))
+
+(def days (vec (for [n (range)
+        :while (not (t/after?
+                      (t/plus (t/date-time 1964 10 10) (t/days n))
+                      (t/date-time 2020 7 24)))]
+  (t/plus (t/date-time 1964 10 10) (t/days n)))))
+
+(defn time-to-str [time]
+  (let [custom-formatter (f/formatter "yyyyMMdd")]
+    (f/unparse custom-formatter time)))
+
+(map binary-string-to-int
+     (filter reverse-same-binary?
+             (map int-to-binary-string
+                  (map parse-int
+                       (map time-to-str days)))))
