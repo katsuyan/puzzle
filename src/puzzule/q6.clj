19c045a16091c00f0b7f7022e567185ac4c8800a
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-04 11:17:53 +0900

    chagne q6

--- src/puzzule/q6.clj
+++ src/puzzule/q6.clj
@@ -4,8 +4,8 @@
   (cond (= n start) true
         (= n 1) false
         :else (if (even? n)
-                  (check-recur (/ n 2) start)
-                  (check-recur (+ (* n 3) 1) start))))
+                  (recur (/ n 2) start)
+                  (recur (+ (* n 3) 1) start))))
 
 (defn check [n]
   (check-recur (+ (* n 3) 1) n))
