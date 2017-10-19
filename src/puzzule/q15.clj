fa01154a5e290b1be809a5c50f917f731b3c831a
Katsuya Tajima <zakichan1216@gmail.com>
2017-10-19 21:10:08 +0900

    fix q15

--- src/puzzule/q15.clj
+++ src/puzzule/q15.clj
@@ -9,8 +9,7 @@
     (> a b) 0
     (= a b) 1
     :else (+ cnt
-             (reduce + (map (fn [n] (apply #(move (+ a %1) (- b %2) cnt) n))
+             (reduce + (map (fn [n] (apply #(move (+ a %) (- b %) cnt) n))
                             [[1 1] [1 2] [1 3] [1 4] [2 1] [2 2] [2 3] [2 4] [3 1] [3 2] [3 3] [3 4] [4 1] [4 2] [4 3] [4 4]])))))
 
 (move 0 n 0)
-(map #(apply + %) (combo/combinations (range 1 5) 2))
