b0df748bbb13ae80e7029c34f48daf8403ae690c
Katsuya Tajima <zakichan1216@gmail.com>
2017-10-19 14:08:05 +0900

    fix q15 tmp

--- src/puzzule/q15.clj
+++ src/puzzule/q15.clj
@@ -1,1 +1,16 @@
-(ns puzzule.q15)
+(ns puzzule.q15
+  (:require [clojure.math.combinatorics :as combo]))
+
+(def n 10)
+(def steps 4)
+
+(defn move [a b cnt]
+  (cond
+    (> a b) 0
+    (= a b) 1
+    :else (+ cnt
+             (reduce + (map (fn [n] (apply #(move (+ a %1) (- b %2) cnt) n))
+                            [[1 1] [1 2] [1 3] [1 4] [2 1] [2 2] [2 3] [2 4] [3 1] [3 2] [3 3] [3 4] [4 1] [4 2] [4 3] [4 4]])))))
+
+(move 0 n 0)
+(map #(apply + %) (combo/combinations (range 1 5) 2))
