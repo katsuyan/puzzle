e76597599ba5459e3cf76849979d6b5ecb270abc
Katsuya Tajima <zakichan1216@gmail.com>
2017-10-25 20:31:30 +0900

    fix q15

--- src/puzzule/q15.clj
+++ src/puzzule/q15.clj
@@ -1,15 +1,18 @@
-(ns puzzule.q15
-  (:require [clojure.math.combinatorics :as combo]))
+(ns puzzule.q15)
 
 (def n 10)
 (def steps 4)
 
-(defn move [a b cnt]
+(defn move-pares [max-num]
+  (for [i (range 1 (inc max-num))
+        j (range 1 (inc max-num))] [i j]))
+
+(defn move [steps a b cnt]
   (cond
     (> a b) 0
     (= a b) 1
     :else (+ cnt
-             (reduce + (map (fn [n] (apply #(move (+ a %) (- b %) cnt) n))
-                            [[1 1] [1 2] [1 3] [1 4] [2 1] [2 2] [2 3] [2 4] [3 1] [3 2] [3 3] [3 4] [4 1] [4 2] [4 3] [4 4]])))))
+             (reduce + (map (fn [move-pare] (apply #(move steps (+ a %1) (- b %2) cnt) move-pare))
+                            (move-pares steps))))))
 
-(move 0 n 0)
+(move steps 0 n 0)
