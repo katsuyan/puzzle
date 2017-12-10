59e24e72ca4fe407a15f4596d7226e4e06b9b4d2
Katsuya Tajima <zakichan1216@gmail.com>
2017-12-10 11:59:14 +0900

    update 1.9.0

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://zakiran.hatenablog.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-time "0.13.0"]
                  [org.clojure/math.combinatorics "0.1.4"]])
