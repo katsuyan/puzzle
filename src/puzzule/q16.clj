ad94befe146c9d867e1f7b1282da79573a325426
Katsuya Tajima <zakichan1216@gmail.com>
2018-02-24 14:57:50 +0900

    fix

--- src/puzzule/q16.clj
+++ src/puzzule/q16.clj
@@ -1,18 +1,8 @@
 (ns puzzule.q16
   (:require [clojure.math.combinatorics :as combo]))
 
-(range 1 (inc 500))
-
-(def line 20)
-(def half-line (/ line 2))
-
-(defn area [x y]
-  (* x y))
-
 (defn area-from-pare [pare]
   (apply * pare))
-
-(area (/ half-line 2) (/ half-line 2))
 
 (defn pares [half-line]
   (defn pares-rec [x y pare-list]
@@ -21,15 +11,17 @@
       (recur (dec x) (inc y) (conj pare-list [x y]))))
   (pares-rec (dec half-line) 1 []))
 
-(pares half-line)
-
-(combo/combinations (pares half-line) 2)
-
-(map #(+ (area-from-pare (first %))
-         (area-from-pare (second %)))
-     (combo/combinations (pares half-line) 2))
-
 (defn include? [seq key]
   (some #(= key %) seq))
 
-(include? [25 30 33] 25)
+(defn eq_filter [half-line half-line-pares]
+  (if (= (area-from-pare [(/ half-line 2) (/ half-line 2)])
+          (+ (area-from-pare (first half-line-pares))
+             (area-from-pare (second half-line-pares))))
+    half-line-pares))
+
+(defn eq_sq? [half-line]
+  (map #(eq_filter half-line %)
+          (combo/combinations (pares half-line) 2)))
+
+(count (distinct (filter #(not (nil? %)) (map eq_sq? (map #(/ % 2) (filter #(= 0 (mod % 4)) (range 1 (inc 500))))))))
